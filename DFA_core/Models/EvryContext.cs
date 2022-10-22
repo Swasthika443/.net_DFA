636bcac427386ae6066b00760956341c25d9caf2
swasthika443 <swasthika2409@gmail.com>
2022-10-22 10:50:13 +0530

    "first commit"

---
+++ DFA_core/Models/EvryContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DFA_core.Models
+{
+    public partial class EvryContext : DbContext
+    {
+        public EvryContext()
+        {
+        }
+
+        public EvryContext(DbContextOptions<EvryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Trainee> Trainees { get; set; } = null!;
+
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trainee>(entity =>
+            {
+                entity.HasKey(e => e.Tid)
+                    .HasName("PK__Trainee__C456D729999D232C");
+
+                entity.ToTable("Trainee");
+
+                entity.Property(e => e.Tid)
+                    .ValueGeneratedNever()
+                    .HasColumnName("TID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("NAME");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
